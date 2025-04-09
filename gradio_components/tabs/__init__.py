8acb8dfd77f9e3e1241e02138190de69c27da58f
Kalhar Pandya <66383494+KalharPandya@users.noreply.github.com>
2025-04-08 23:38:14 -0700

    Update tabs package to properly expose modules

--- gradio_components/tabs/__init__.py
+++ gradio_components/tabs/__init__.py
@@ -1,1 +1,43 @@
 # Tabs package for Gradio interface
+
+# Make the individual tab modules accessible
+__all__ = [
+    'about',
+    'compress', 
+    'decompress',
+    'analysis',
+    'file_format',
+    'help'
+]
+
+# Import the modules so they can be accessed directly from the tabs package
+# Using try/except for each to avoid cascading failures
+try:
+    from . import about
+except ImportError:
+    pass
+
+try:
+    from . import compress
+except ImportError:
+    pass
+
+try:
+    from . import decompress
+except ImportError:
+    pass
+
+try:
+    from . import analysis
+except ImportError:
+    pass
+
+try:
+    from . import file_format
+except ImportError:
+    pass
+
+try:
+    from . import help
+except ImportError:
+    pass
